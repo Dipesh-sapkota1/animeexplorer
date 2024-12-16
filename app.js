9e304f000e3a19606167b166eae4eba7c9c7c3bb
Dipesh-sapkota1 <sapkotadipes.com>
2024-12-16 23:03:03 +0545

    added path module and debugging middleware

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-import {} from 'dotenv/config';
+import 'dotenv/config';
 import express from 'express';
 import router from './routes/anime.Routes.js';
 import cors from 'cors';
+import path from 'path';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());    
+app.use(cors());
 
 // Set the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
 // Set the static folder
-app.use(express.static("public"));
+app.use(express.static(path.join(process.cwd(), 'public')));
 
-
- 
+// Root route (optional)
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 
 // Routes
 app.use('/anime', router);
